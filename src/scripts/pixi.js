54097682ea89e263abae1e23b3a8eb249cbc5571
Chuong Le <redocecin@gmail.com>
2020-10-16 15:13:05 +0700

    support Hi-DPI for pixi benchmark

--- src/scripts/pixi.js
+++ src/scripts/pixi.js
@@ -5,14 +5,19 @@ class PixiEngine extends Engine {
   constructor() {
     super();
 
+    // support Hi-DPI
+    this.canvasWidth = this.width * window.devicePixelRatio;
+    this.canvasHeight = this.height * window.devicePixelRatio;
     this.rects = [];
     this.app = new PIXI.Application({
-      width: this.width,
-      height: this.height,
+      width: this.canvasWidth,
+      height: this.canvasHeight,
       backgroundColor: 0xFFFFFF,
       antialias: true,
     });
     this.content.appendChild(this.app.view);
+    this.app.view.style.width = this.width + "px";
+    this.app.view.style.height = this.height + "px";
   }
 
   onTick() {
@@ -26,7 +31,7 @@ class PixiEngine extends Engine {
     }
 
     rectsToRemove.forEach(i => {
-      this.rects[i].x = this.width + this.rects[i].size / 2;
+      this.rects[i].x = this.canvasWidth + this.rects[i].size / 2;
     });
 
     this.meter.tick();
@@ -37,13 +42,13 @@ class PixiEngine extends Engine {
     this.app.stage.removeChildren();
     this.rects = {};
     for (let i = 0; i < this.count.value; i++) {
-      const x = Math.random() * this.width;
-      const y = Math.random() * this.height;
-      const size = 10 + Math.random() * 40;
+      const x = Math.random() * this.canvasWidth;
+      const y = Math.random() * this.canvasHeight;
+      const size = (10 + Math.random() * 40) * window.devicePixelRatio;
       const speed = 1 + Math.random();
 
       const rect = new PIXI.Graphics();
-      rect.lineStyle(1, 0x000000, 1);
+      rect.lineStyle(window.devicePixelRatio, 0x000000, 1);
       rect.beginFill(0xffffff);
       rect.drawRect(-size / 2, -size / 2, size, size);
       rect.endFill();
