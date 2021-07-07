8dffd950d529c8ef93b1225137720e2f387146bc
SlaneYang <slaneyang@gmail.com>
2021-07-07 22:09:02 +0800

    feat: Add CanvasKit and mesh.js

---
+++ src/scripts/canvaskit.js
@@ -0,0 +1,86 @@
+import Engine from './engine';
+import CanvasKitInit from 'canvaskit-wasm';
+import CanvasKitPkg from 'canvaskit-wasm/package.json';
+
+window.cancelRequestAnimFrame = (() => {
+	return window.cancelAnimationFrame ||
+		window.webkitCancelRequestAnimationFrame ||
+		window.mozCancelRequestAnimationFrame ||
+		window.oCancelRequestAnimationFrame ||
+		window.msCancelRequestAnimationFrame ||
+		clearTimeout;
+})();
+
+const wasmRemotePath = `https://unpkg.com/canvaskit-wasm@${CanvasKitPkg.version}/bin/`;
+
+class CanvasKitEngine extends Engine {
+	constructor() {
+		super();
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+		this.content.appendChild(this.canvas);
+	}
+
+	async init() {
+		this.CanvasKit = await CanvasKitInit({
+			locateFile: (file) => `${wasmRemotePath}${file}`,
+		})
+        this.surface = this.CanvasKit.MakeWebGLCanvasSurface(this.canvas);
+        this.strokePaint = new this.CanvasKit.Paint();
+        this.strokePaint.setAntiAlias(true);
+        this.strokePaint.setColor(this.CanvasKit.parseColorString('#000000'));
+        this.strokePaint.setStyle(this.CanvasKit.PaintStyle.Stroke);
+        this.strokePaint.setStrokeWidth(2.0);
+        this.fillPaint = new this.CanvasKit.Paint();
+        this.fillPaint.setAntiAlias(true);
+        this.fillPaint.setColor(this.CanvasKit.parseColorString('#ffffff'));
+	}
+
+	requestAnimFrame() {
+		const rects = this.rects;
+        const canvas = this.surface.getCanvas();
+		for (let i = 0; i < this.count.value; i++) {
+			const r = rects[i];
+			r.x -= r.speed;
+			if (r.x + r.size < 0) {
+				r.x = this.width + r.size;
+			}
+            canvas.drawRect4f(r.x, r.y, r.x + r.size, r.y + r.size, this.strokePaint);
+            canvas.drawRect4f(r.x, r.y, r.x + r.size, r.y + r.size, this.fillPaint);
+		}
+		this.surface.flush();
+		this.meter.tick();
+
+		this.request = window.requestAnimationFrame(() => {
+            this.requestAnimFrame()
+        });
+	}
+
+	render() {
+		// clear the canvas
+		this.surface.getCanvas().clear(this.CanvasKit.WHITE);
+		window.cancelRequestAnimFrame(this.request);
+
+		// rectangle creation
+		const rects = new Array(this.count);
+		for (let i = 0; i < this.count.value; i++) {
+			const x = Math.random() * this.width;
+			const y = Math.random() * this.height;
+			const size = 10 + Math.random() * 40;
+			const speed = 1 + Math.random();
+			rects[i] = { x, y, size, speed };
+		}
+		this.rects = rects;
+
+		this.request = window.requestAnimationFrame(() => {
+			this.requestAnimFrame()
+		});
+	};
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+	const engine = new CanvasKitEngine();
+	await engine.init();
+	engine.render();
+});
---
+++ src/scripts/mesh.js
@@ -0,0 +1,80 @@
+import Engine from './engine';
+import { Renderer, Figure2D, Mesh2D } from '@mesh.js/core';
+
+window.cancelRequestAnimFrame = (() => {
+	return window.cancelAnimationFrame ||
+		window.webkitCancelRequestAnimationFrame ||
+		window.mozCancelRequestAnimationFrame ||
+		window.oCancelRequestAnimationFrame ||
+		window.msCancelRequestAnimationFrame ||
+		clearTimeout;
+})();
+
+class MeshEngine extends Engine {
+	constructor() {
+		super();
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+		this.content.appendChild(this.canvas);
+	}
+
+	async init() {
+        this.renderer = new Renderer(this.canvas);
+	}
+
+	requestAnimFrame() {
+		const rects = this.rects;
+		for (let i = 0; i < this.count.value; i++) {
+			const r = rects[i];
+			if (r.x + r.mesh.transformMatrix[4] + r.size < 0) {
+                r.mesh.translate(this.width + r.size, 0);
+			} else {
+                r.mesh.translate(-r.speed, 0);
+            }
+		}
+        const meshes = rects.map(rect => rect.mesh);
+		this.renderer.drawMeshes(meshes);
+		this.meter.tick();
+
+		this.request = window.requestAnimationFrame(() => {
+			this.requestAnimFrame()
+		});
+	}
+
+	render() {
+		// clear the canvas
+		this.renderer.clear();
+		window.cancelRequestAnimFrame(this.request);
+
+		// rectangle creation
+		const rects = new Array(this.count);
+		for (let i = 0; i < this.count.value; i++) {
+			const x = Math.random() * this.width;
+			const y = Math.random() * this.height;
+			const size = 10 + Math.random() * 40;
+			const speed = 1 + Math.random();
+            const figure = new Figure2D();
+            figure.rect(x, y, size, size);
+            const mesh = new Mesh2D(figure, this.canvas);
+            mesh.setFill({
+            	color: [1, 1, 1, 1],
+            });
+            mesh.setStroke({
+            	color: [0, 0, 0, 1],
+            });
+			rects[i] = { x, y, size, speed, mesh };
+		}
+		this.rects = rects;
+
+		this.request = window.requestAnimationFrame(() => {
+			this.requestAnimFrame()
+		});
+	};
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+	const engine = new MeshEngine();
+	await engine.init();
+	engine.render();
+});
