7e3943e896ca593d6d28f58c5c04f1c9e92d27c8
wouterlucas <wouter@wouterlucas.com>
2025-06-27 16:48:22 +0200

    refactor(lightningjs): update renderer initialization and animation handling

--- src/scripts/lightningjs.js
+++ src/scripts/lightningjs.js
@@ -1,35 +1,52 @@
 import Engine from "./engine";
-import { RendererMain, MainCoreDriver } from "@lightningjs/renderer/dist/exports/main-api";
+import { RendererMain } from '@lightningjs/renderer/dist/exports/index.js';
+import { WebGlCoreRenderer, SdfTextRenderer } from '@lightningjs/renderer/dist/exports/webgl.js';
 
 class Lightning extends Engine {
   constructor() {
     super();
 
     this.rects = [];
-    this.renderer = new RendererMain(
-      {
-        appWidth: this.width,
-        appHeight: this.height,
-        clearColor: 0xffffffff,
-      },
-      this.content,
-      new MainCoreDriver(),
-    );
-  }
+    this.animations = [];
+    
+    // Create and append the app div
+    this.appDiv = document.createElement('div');
+    this.appDiv.id = 'app';
+    this.appDiv.style.width = this.width + 'px';
+    this.appDiv.style.height = this.height + 'px';
+    this.content.appendChild(this.appDiv);
+    
+    this.renderer = new RendererMain({
+      appWidth: this.width,
+      appHeight: this.height,
+      clearColor: 0x00000000,
+      numImageWorkers: 0, // Disable image workers for simplicity
+      renderEngine: WebGlCoreRenderer,
+      fontEngines: [ SdfTextRenderer ]
+    }, 'app');
 
-  animate() {
-    this.meter.tick();
-    this.request = requestAnimationFrame(() => this.animate());
+    this.renderer.on('frameTick', () => {
+      this.meter.tick();
+    });
   }
 
   async render() {
-    this.cancelAnimationFrame(this.request);
-    await this.renderer.init();
+    // Stop any existing animations
+    this.animations.forEach(animation => {
+      if (animation) {
+        animation.stop();
+      }
+    });
+    this.animations = [];
+    
+    // Clean up existing nodes
     for (let i = 0; i < this.rects.length; i++) {
       this.renderer.destroyNode(this.rects[i]);
     }
 
-    this.rects = new Array(this.count);
+    this.rects = new Array(this.count.value);
+    this.animations = new Array(this.count.value);
+    
     for (let i = 0; i < this.count.value; i++) {
       const x = Math.random() * this.width;
       const y = Math.random() * this.height;
@@ -55,33 +72,48 @@ class Lightning extends Engine {
         }),
       });
 
-      let animation;
-      const firstDuration = (x + size) / speed;
-      const secondDuration = (this.width + size) / speed;
-      let init = true;
-
-      (async () => {
-        while (true) {
-          animation = node
-            .animate(
-              {
-                x: 0 - size,
-              },
-              {
-                duration: init ? firstDuration : secondDuration,
-              }
-            )
-            .start();
-          await animation.waitUntilStopped();
-          if (init) init = false;
-          node.x = this.width;
-        }
-      })();
-
       this.rects[i] = node;
+      
+      // Start the animation loop for this node
+      this.startNodeAnimation(node, size, speed, i);
     }
+  }
 
-    this.request = requestAnimationFrame(() => this.animate());
+  startNodeAnimation(node, size, speed, index) {
+    const firstDuration = (node.x + size) / speed;
+    const secondDuration = (this.width + size) / speed;
+    let isFirstAnimation = true;
+
+    const animateToLeft = () => {
+      const duration = isFirstAnimation ? firstDuration : secondDuration;
+      
+      const animation = node.animate(
+        {
+          x: 0 - size,
+        },
+        {
+          duration: duration,
+          easing: 'linear',
+          loop: false,
+        }
+      );
+
+      animation.start();
+      this.animations[index] = animation;
+
+      // When animation finishes, reset position and start again
+      animation.waitUntilStopped().then(() => {
+        if (this.animations[index] === animation) { // Check if this is still the current animation
+          node.x = this.width;
+          isFirstAnimation = false;
+          animateToLeft(); // Start the next animation
+        }
+      }).catch(() => {
+        // Animation was stopped/cancelled, don't continue
+      });
+    };
+
+    animateToLeft();
   }
 }
 
